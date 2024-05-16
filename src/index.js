46408b58cae1da72e1f1b0a2eac3acb525265f3b
ItsKandar <nmoreau15@myges.fr>
2024-05-16 08:11:38 +0200

    removed stuff from index.js

--- src/index.js
+++ src/index.js
@@ -15,16 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-var express = require('express');
-var app = express();
-var port = 4000;
-
-app.get("/", async (req, res ) => {
-  res.end(JSON.stringify({message: "Hello World"}));
-} );
-
-app.listen(port, () => {
-  console.log("Server listening on port " + port);
-  console.log( 'press CTRL+C to exit')
-});
